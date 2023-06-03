a603b6442ab773f04414afc3f51af5ada0f8e54f
Neal Geilen <info@nealgeilen.nl>
2023-06-03 11:03:26 +0200

    Frontend style fixes

--- Epsilon.Host.Frontend/src/components/Competance/PersonalDevelopmentMatrix.vue
+++ Epsilon.Host.Frontend/src/components/Competance/PersonalDevelopmentMatrix.vue
@@ -1,89 +1,87 @@
 <template>
-  <apexcharts
-    type="bar"
-    height="320"
-    width="300"
-    :options="chartOptions"
-    :series="series"
-  />
+    <apexcharts
+        type="bar"
+        height="320"
+        width="300"
+        :options="chartOptions"
+        :series="series" />
 </template>
 
 <script lang="ts" setup>
-import apexcharts from "vue3-apexcharts";
-import {HboIDomain, ProfessionalTaskOutcome} from "@/logic/Api";
+import apexcharts from "vue3-apexcharts"
+import { HboIDomain, ProfessionalTaskOutcome } from "@/logic/Api"
 
 const props = defineProps<{
     domain: HboIDomain
     data: ProfessionalTaskOutcome[]
 }>()
 
-const series = [{
-    name: '',
-    data: [44, 55, 41, 67]
-}
+const series = [
+    {
+        name: "",
+        data: [44, 55, 41, 67],
+    },
 ]
 const chartOptions = {
     annotations: {
         yaxis: [
             {
                 y: 40,
-                borderColor: 'red',
+                borderColor: "red",
                 strokeDashArray: 0,
                 label: {
-                    borderColor: 'red',
+                    borderColor: "red",
                     style: {
-                        color: '#fff',
-                        background: 'red'
+                        color: "#fff",
+                        background: "red",
                     },
-                    text: 'Mastery'
-                }
-            }
-        ]
+                    text: "Mastery",
+                },
+            },
+        ],
     },
     colors: ["#A8D08D"],
     chart: {
-        type: 'bar',
+        type: "bar",
         stacked: true,
         toolbar: {
-            show: false
+            show: false,
         },
         zoom: {
-            enabled: false
-        }
+            enabled: false,
+        },
     },
     dataLabels: {
-        enabled: false
+        enabled: false,
     },
     plotOptions: {
         bar: {
             horizontal: false,
             borderRadius: 4,
-            dataLabels: {}
+            dataLabels: {},
         },
     },
     xaxis: {
-        type: 'string',
+        type: "string",
         categories: [],
     },
     yaxis: {
         show: false,
     },
     legend: {
-        position: 'bottom'
+        position: "bottom",
     },
     fill: {
-        opacity: 1
+        opacity: 1,
     },
     tooltip: {
-        enabled: false
-    }
+        enabled: false,
+    },
 }
 
-props.domain.professionalSkills.forEach(skill =>{
+props.domain.professionalSkills.forEach((skill) => {
     chartOptions.xaxis.categories.push(skill.shortName)
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
